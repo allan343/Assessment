783d40474932aef556bb2cc8fcab317e638cc79f
Allan Cheng <allanhfcheng@gmail.com>
2021-08-03 00:35:43 -0700

    added last name filtering

--- src/StudentList/StudentList.js
+++ src/StudentList/StudentList.js
@@ -3,12 +3,11 @@ import Student from '../Student/Student';
 import './StudentList.css';
 
 class StudentList extends Component {
-  
 
   constructor(props) {
     super(props);
     this.state = {
-     
+
       name: {
         value: "",
         touched: false
@@ -17,30 +16,32 @@ class StudentList extends Component {
   }
   static defaultProps = {
     students: [],
-  
+
   };
   updateName(name) {
     this.setState({ name: { value: name, touched: true } });
   }
-  
-  render() {
- 
- const {students}=this.props;
- let filteredStudents = [];
- if (this.state.name.value == "") {
-  filteredStudents = students;
-}
-else {
-  filteredStudents = students.filter(student => student.firstName.toLowerCase().includes(this.state.name.value.toLowerCase()));
-}
 
-  
+  render() {
+
+    const { students } = this.props;
+    let filteredStudents = [];
+    if (this.state.name.value == "") {
+      filteredStudents = students;
+    }
+    else {
+      filteredStudents = students.filter(student => student.firstName.toLowerCase().includes(this.state.name.value.toLowerCase()) ||
+        student.lastName.toLowerCase().includes(this.state.name.value.toLowerCase())
+      );
+    }
+
+
     return (
-        <div className="studentList">
-           <input type="text" className="folder__control" placeholder=""
-            name="name" id="name" value={this.state.name.value} onChange={e => this.updateName(e.target.value)} />
+      <div className="studentList">
+        <input type="text" className="folder__control" placeholder=""
+          name="name" id="name" value={this.state.name.value} onChange={e => this.updateName(e.target.value)} />
         <ul className='StudentList__list' aria-live='polite'>
-            
+
           {filteredStudents.map(student =>
 
             <li key={student.id} >
