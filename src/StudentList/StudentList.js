b66119f680d57b020a8d4064cdf3d853f1e0bc7b
Allan Cheng <allanhfcheng@gmail.com>
2021-08-03 00:32:35 -0700

    added firstname filter

--- src/StudentList/StudentList.js
+++ src/StudentList/StudentList.js
@@ -4,22 +4,44 @@ import './StudentList.css';
 
 class StudentList extends Component {
   
+
+  constructor(props) {
+    super(props);
+    this.state = {
+     
+      name: {
+        value: "",
+        touched: false
+      }
+    };
+  }
   static defaultProps = {
     students: [],
   
   };
-
+  updateName(name) {
+    this.setState({ name: { value: name, touched: true } });
+  }
+  
   render() {
  
  const {students}=this.props;
-
+ let filteredStudents = [];
+ if (this.state.name.value == "") {
+  filteredStudents = students;
+}
+else {
+  filteredStudents = students.filter(student => student.firstName.toLowerCase().includes(this.state.name.value.toLowerCase()));
+}
 
   
     return (
         <div className="studentList">
+           <input type="text" className="folder__control" placeholder=""
+            name="name" id="name" value={this.state.name.value} onChange={e => this.updateName(e.target.value)} />
         <ul className='StudentList__list' aria-live='polite'>
             
-          {students.map(student =>
+          {filteredStudents.map(student =>
 
             <li key={student.id} >
               <Student
