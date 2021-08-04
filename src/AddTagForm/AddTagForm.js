521ae22be1e3476cb53cc3ad9de9852f22a16a41
Allan Cheng <allanhfcheng@gmail.com>
2021-08-04 00:47:54 -0700

    handling add tag

--- src/AddTagForm/AddTagForm.js
+++ src/AddTagForm/AddTagForm.js
@@ -2,34 +2,39 @@ import React from "react";
 
 export default class AddItemForm extends React.Component {
 
-  
+
   static defaultProps = {
     tags: []
   };
   render() {
-    let {tags} = this.props
+    let { tags } = this.props
     return (
       <div>
-      <form>
-        <input
-          type='text'
-          placeholder='Add Tag'
-          aria-label='Shopping list item'
-        />
-        <button type='submit'>Add Tag</button>
-      </form>
+        <form>
+          <input
+            type='text'
+            placeholder='Add Tag'
+            aria-label='Shopping list item'
+          />
+          
+          <button
+            onClick={() => this.props.handleAddTag()}
+            type='button'>
+            Add Tag
+          </button>
+        </form>
 
-      <div className="tagList">
-      
+        <div className="tagList">
+
           {tags.map(tag =>
 
             <span>tag</span>
           )}
-      
-      </div>
+
+        </div>
       </div>
 
-      
+
     )
   }
 }
