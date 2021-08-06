66e324163f0d46eba5f80c65158654fc20f85ad5
Allan Cheng <allanhfcheng@gmail.com>
2021-08-05 23:13:56 -0700

    check

--- src/StudentList/StudentList.js
+++ src/StudentList/StudentList.js
@@ -34,70 +34,82 @@ class StudentList extends Component {
 
   }
 
-  addTagsToFilter = (tag,id) => {
+  addTagsToFilter = (tag, id) => {
     const newTags = [
       ...this.state.tagsToFilter,
-        {tag,id}
+      {
+        "tag": tag,
+        "id": id
+      }
     ]
     this.setState({
       tagsToFilter: newTags
     })
-};
+  };
 
-getTagsToFilter =()=>{
-  console.log("tagstoFilter",this.state.tagsToFilter);
-  return this.state.tagsToFilter;
-}
+  getTagsToFilter = () => {
+    console.log("tagstoFilter", this.state.tagsToFilter);
+    return this.state.tagsToFilter;
+  }
 
   render() {
-console.log("this.state",this.props.students);
-console.log("tagstoFilterstate",this.state.tagsToFilter)
+    console.log("this.state", this.props.students);
+    console.log("tagstoFilterstate", this.state.tagsToFilter)
     const { students } = this.props;
     let filteredStudents = [];
-    if (this.state.name.value === "" && this.state.tagName.value ==="") {
+    if (this.state.name.value === "" && this.state.tagName.value === "") {
       filteredStudents = students;
     }
     else {
+      let tagQuery = this.state.tagName.value.toLowerCase();
+
+      let filteredIds = this.state.tagsToFilter.filter(function (el) {
+        return el.tag === tagQuery;
+      }
+
+      );
+      console.log("filteredId's", filteredIds);
+      let idsonly = []
+
+      for (let i = 0; i < filteredIds.length; i++) {
+
+        idsonly.push(filteredIds[i].id)
+
+      }
+      console.log("idsonly", idsonly)
+
       filteredStudents = students.filter(
         student => {
-         // console.log("tags", this.state.tag.value.toLowerCase())
-         // console.log("name", student.tags)
-          
-         // let test = student.setTagsToFilter;
-         // let tags = this.context.getTagsToFilter();
-          //console.log("test", tags.includes(this.state.tag.value.toLowerCase()))
           return student.firstName.toLowerCase().includes(this.state.name.value.toLowerCase()) ||
-            student.lastName.toLowerCase().includes(this.state.name.value.toLowerCase() )
-            /*||
-              tags.includes(this.state.tag.value.toLowerCase()))*/
-
+            student.lastName.toLowerCase().includes(this.state.name.value.toLowerCase()) ||
+            idsonly.includes(student.id)
         })
     }
     const value = {
       addTagsToFilter: this.addTagsToFilter,
-  getTagsToFilter:this.getTagsToFilter
+      getTagsToFilter: this.getTagsToFilter
     }
 
     return (
       <ApiContext.Provider value={value}>
-      <div className="studentList">
-        <input type="text" className="nameFilter" placeholder="Search By Name"
-          name="name" id="name" value={this.state.name.value} onChange={e => this.updateName(e.target.value)} />
-        <input type="text" className="tagFilter" placeholder="Search By Tag"
-          name="name" id="name" value={this.state.tagName.value} onChange={e => this.updateTag(e.target.value)} />
-        <ul className='StudentList__list' aria-live='polite'>
-          <p>{this.state.tagName.value}</p>
-          {filteredStudents.map(student =>
+        <div className="studentList">
+          <input type="text" className="nameFilter" placeholder="Search By Name"
+            name="name" id="name" value={this.state.name.value} onChange={e => this.updateName(e.target.value)} />
+          <input type="text" className="tagFilter" placeholder="Search By Tag"
+            name="name" id="name" value={this.state.tagName.value} onChange={e => this.updateTag(e.target.value)} />
+          <ul className='StudentList__list' aria-live='polite'>
+            <p>{this.state.tagName.value}</p>
+            {filteredStudents.map(student =>
 
-            <li key={student.id} >
-              <Student
-                key={student.id}
-                {...student}
-              />
-            </li>
-          )}
-        </ul>
-      </div>
+              <li key={student.id} >
+                <Student
+                  key={student.id}
+                  {...student}
+                />
+              </li>
+            )}
+          </ul>
+        </div>
       </ApiContext.Provider>
     );
   }
